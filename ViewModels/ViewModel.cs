3b520ba36724f1ad1018182e735e6c82ef5eed19
atif-dev <ati1254@gmail.com>
2023-09-08 23:02:54 +0500

    Friday>some icons and arrangements

---
+++ ViewModels/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ChatApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.ViewModels
+{
+    public class ViewModel
+    {
+        public ObservableCollection<StatusDataModel> statusThumbsCollection;
+        public ViewModel()//constructor
+        {
+
+            statusThumbsCollection = new ObservableCollection<StatusDataModel>()
+            {
+                //Since we want to keep first status blank for the user to add own status
+                new StatusDataModel
+                {
+                    IsMeAddStatus = true,
+                },
+                new StatusDataModel
+                {
+                    ContactName="Mike",
+                    ContactPhoto=new Uri("/assets/1.png", UriKind.RelativeOrAbsolute),
+                    StatusImage=new Uri("/assets/5.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus=false
+                },
+                new StatusDataModel
+                {
+                    ContactName="Steve",
+                    ContactPhoto=new Uri("/assets/2.png", UriKind.RelativeOrAbsolute),
+                    StatusImage=new Uri("/assets/8.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus=false
+                },
+                new StatusDataModel
+                {
+                    ContactName="Will",
+                    ContactPhoto=new Uri("/assets/3.png", UriKind.RelativeOrAbsolute),
+                    StatusImage=new Uri("/assets/5.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus=false
+                },
+
+            };
+
+        }
+    }
+}
